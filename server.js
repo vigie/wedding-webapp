892115b12acea39cd34955e9cf18376fca7a8bb9
Matt Critchlow <mcritch@vmware.com>
2016-04-15 23:13:19 -0700

    security & cleanup WIP

--- server.js
+++ server.js
@@ -1,44 +1,18 @@
-/**
- * @license
- * Everything in this repo is MIT License unless otherwise specified.
- *
- * Copyright (c) Addy Osmani, Sindre Sorhus, Pascal Hartig, Stephen  Sawchuk, Google, Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 // set up ========================
 var express  = require('express');
-var app      = express(); 								// create our app w/ express
-var mongoose = require('mongoose'); 					// mongoose for mongodb
+var app      = express(); 
+var mongoose = require('mongoose'); 
+var Schema = mongoose.Schema;
 var morgan = require('morgan'); 			// log requests to the console (express4)
 var bodyParser = require('body-parser'); 	// pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var argv = require('optimist').argv;
-var Sendgrid = require('sendgrid')(
-  process.env.SENDGRID_API_KEY || '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'
-);
+var Sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
 // configuration =================
 
 mongoose.connect('mongodb://' + argv.be_ip + ':80/my_database');
 
-app.use(express.static(__dirname));
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
@@ -48,8 +22,8 @@ app.use(bodyParser.json()); 									// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-// define model =================
-var Guest = mongoose.model('Guest', {
+// Define Schema
+var GuestSchema = new Schema({
     _id: String,
     firstName: String,
     lastName: String,
@@ -61,8 +35,21 @@ var Guest = mongoose.model('Guest', {
     sc_invite: String,
     gsy_response: String,
     sf_response: String,
-    sc_response: String
- });
+    sc_response: String,
+    address: String,
+    phone: String,
+    relationship: String,
+    domiciled: String    
+});
+
+// Exclude sensitive fields not used by webapp
+GuestSchema.path('address').select(false);
+GuestSchema.path('phone').select(false);
+GuestSchema.path('relationship').select(false);
+GuestSchema.path('domiciled').select(false);
+
+// define model =================
+var Guest = mongoose.model('Guest', GuestSchema);
 
 // routes ======================================================================
 
@@ -187,89 +174,25 @@ app.get('/api/guests', function(req, res) {
     }
 });
 
-// Get all todos
-app.get('/api/todos', function(req, res) {
-    Todo.find(function(err, todos) {
-
-            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-            if (err)
-                res.send(err)
-
-            res.json(todos); // return all todos in JSON format
-        });
-
-});
-
-
-// // create todo and send back all todos after creation
-// app.post('/api/todos', function(req, res) {
-
-// 	// create a todo, information comes from AJAX request from Angular
-// 	Todo.create({
-// 		title : req.body.title,
-// 		completed : false
-// 	}, function(err, todo) {
-// 		if (err)
-// 			res.send(err);
-
-// 		// get and return all the todos after you create another
-// 		Todo.find(function(err, todos) {
-// 			if (err)
-// 				res.send(err)
-// 			res.json(todos);
-// 		});
-// 	});
-
-// });
-
-// app.put('/api/todos/:todo_id', function(req, res){
-//   return Todo.findById(req.params.todo_id, function(err, todo) {
-//     todo.title = req.body.title;
-//     todo.completed = req.body.completed;
-//     return todo.save(function(err) {
-//       if (err) {
-//         res.send(err);
-//       }
-//       return res.send(todo);
-//     });
-//   });
-// });
-
 app.get('/api/guests/:guest_id', function(req, res){
   return Guest.findById(req.params.guest_id, function(err, guest) {
     if (err) {
         res.send(err);
     }
+    console.log(guest);
     return res.send(guest);
   });
 });
 
-// // delete a todo
-// app.delete('/api/todos/:todo_id', function(req, res) {
-// 	Todo.remove({
-// 		_id : req.params.todo_id
-// 	}, function(err, todo) {
-// 		if (err)
-// 			res.send(err);
-
-// 		// get and return all the todos after you create another
-// 		Todo.find(function(err, todos) {
-// 			if (err)
-// 				res.send(err)
-// 			res.json(todos);
-// 		});
-// 	});
-// });
-
 // application -------------------------------------------------------------
 app.get('/', function(req, res) {
-    res.sendfile('index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendfile('app/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 // Handle html5mode
 app.get('/*', function(req, res) { 
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile('app/index.html');
 });
 
 // listen (start app with node server.js) ======================================
-app.listen(8080, argv.fe_ip);
-console.log("App listening on port 8080");
+app.listen(80, argv.fe_ip);
+console.log("App listening on port 80");
